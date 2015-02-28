381bf53497bfd2e4d5330357b03f7e63461c0399
Peihong Li <peihong.li@shopify.com>
2015-02-28 09:11:42 -0800

    user creation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   def create
     facebook = User.koala(request.env['omniauth.auth']['credentials'])
     facebook_user = facebook.get_object("me")
-    @user = User.where(email: facebook_user["email"]).first_or_create
+    @user = User.find_or_create_by(email: facebook_user["email"])
     session[:user_id] = @user.id
     redirect_to root_url
   end
