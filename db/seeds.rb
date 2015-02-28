5e451f6867b6249ba3a0bc19429597ba8f4763ca
Peihong Li <peihong.li@shopify.com>
2015-02-28 13:17:46 -0800

    spelling mistake in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-user = User.create(first_name: 'Pei', last_name: 'Li', email: 'peihongl94@gmail.com')
+user = User.create(first_name: 'Pei', last_name: 'Li', email: 'peihongli94@gmail.com')
 commitment = Commitment.create(title: "Shave Pei's Head", description: "We're gonna shave Pei.", expiry_date: DateTime.now, status: 'pending', cost: 5)
 sponsorship = user.sponsorships.create(cost: 1, sponsorship_type: 'ownership', commitment_id: commitment.id)
