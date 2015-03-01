ceab92d7812c2d1b4f336769f5f0a2fa7cde315e
Peihong Li <peihong.li@shopify.com>
2015-03-01 12:41:02 -0800

    sandbox

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def coinbase
-    @coinbase ||= Coinbase::Client.new(COINBASE_CONFIG['COINBASE_APP_KEY'], COINBASE_CONFIG['COINBASE_APP_SECRET'])
+    @coinbase ||= Coinbase::Client.new(COINBASE_CONFIG['COINBASE_APP_KEY'], COINBASE_CONFIG['COINBASE_APP_SECRET'], base_uri: "https://api.sandbox.coinbase.com/v1")
   end
 
   def cb_bank
